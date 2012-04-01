f19d3413785fb7f612fa2af14d04464f610a5ba5
Fred Dubois <fred.dubois@gmail.com>
2012-04-01 05:26:53 -0400

    setup.sh now creates pre and post vimrcs

--- setup.sh
+++ setup.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-cd ~
-if [[ -e .vimrc ]]; then
-	print "backing up .vimrc to .vimrc.old"
-	mv .vimrc .vimrc.old
-fi
+timestamp=`date +%Y-%m-%d_%H:%m`
 
+cd ~
+
+backup_file() {
+	file_to_backup=$1
+	if [[ -e $file_to_backup ]]; then
+		backupfile="${file_to_backup}.${timestamp}.old"
+		mv -v $file_to_backup $backupfile
+	fi
+}
+
+backup_file ~/.vimrc
 ln -s ~/.vim/vimrc .vimrc
 
+if [[ ! -e ~/.vimrc.pre ]]; then
+	touch ~/.vimrc.pre
+fi
+
+if [[ ! -e ~/.vimrc.post ]]; then
+	touch ~/.vimrc.post
+fi
+
+print 'done!'
